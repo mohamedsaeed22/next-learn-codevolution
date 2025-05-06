2babe7cf6025431d8570291b544f004756aa10c5
mohamedsaeed22 <mohamed.saeed1006@gmail.com>
2025-05-06 14:57:45 +0300

    metadata

--- src/app/products/[productId]/page.tsx
+++ src/app/products/[productId]/page.tsx
@@ -1,10 +1,20 @@
- 
-const ProductDetails = async ({
+import { Metadata } from "next";
+
+type ProductDetailsProps = {
+  params: { productId: string };
+};
+
+export const generateMetadata = async ({
   params,
 }: {
-  params: Promise<{ productId: string }>;
-}) => {
-  const productId = (await params).productId;
+  params: { productId: string };
+}): Promise<Metadata> => {
+  const id = params.productId;
+  return { title: `Product ${id}` };
+};
+
+const ProductDetails = async ({ params }: ProductDetailsProps) => {
+  const productId = params.productId;
   return <div>this is details about product {productId}</div>;
 };
 
